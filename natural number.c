6862ed5c6316d508a5caa898e52b0c0f4b8856af
AngadShani <163164176+AngadShani@users.noreply.github.com>
2024-03-24 09:42:18 +0530

    Add files via upload

---
+++ natural number.c
@@ -0,0 +1,17 @@
+// check if given number is a natural number or not
+#include <stdio.h>
+ int main()
+ {
+   int number;
+   printf("enter the number: ");
+   scanf("%d", & number);
+   if (number >= 1){
+     printf("its a natural number.");
+   }
+   else {
+     printf("its not a natural number");
+   }
+ }
+ 
+ 
+ 
